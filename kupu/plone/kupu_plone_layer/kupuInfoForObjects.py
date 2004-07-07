1e0640c8d2f6cc3aa86435a59ce6b41b3a439446
duncan <kupuguy@gmail.com>
2004-07-07 15:59:37 +0000

    Added preview for images

--- kupu/plone/kupu_plone_layer/kupuInfoForObjects.py
+++ kupu/plone/kupu_plone_layer/kupuInfoForObjects.py
@@ -11,6 +11,17 @@ kupu_tool = getToolByName(context, 'kupu_library_tool')
 coll_types = kupu_tool.queryPortalTypesForResourceType('collection', ())
 preview_action = 'kupupreview'
 
+def maybeGetAttr(obj, name):
+    '''Get an attribute if it exists.
+    Call it if callable
+    '''
+    if not hasattr(obj, name):
+        return None
+    val = getattr(obj, name)
+    if callable(val):
+        return val()
+    return val
+
 # A list comprehension might seem a little cryptic here (although it
 # should be readable just fine). The reason we use it is that it is
 # much faster instead of iterating and appending manually.
@@ -19,7 +30,9 @@ items = [{'id':          obj.getId(),
           'portal_type': obj.portal_type,
           'collection':  obj.portal_type in coll_types,
           'icon':        context.portal_url() + '/' + obj.getIcon(),
-          'size':        hasattr(obj, 'get_size') and obj.get_size() or None,
+          'size':        hasattr(obj, 'get_size') and context.getObjSize(obj) or None,
+          'width':       maybeGetAttr(obj, 'width'),
+          'height':      maybeGetAttr(obj, 'height'),
           'preview':     obj.getTypeInfo().getActionById(preview_action, None),
           'title':       obj.Title() or obj.getId(),
           'description': obj.Description()} for obj in values]
