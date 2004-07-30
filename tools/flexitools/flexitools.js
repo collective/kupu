2f02d01467dc05010817ba8a802334e167eb9524
guido <guido>
2004-07-30 15:16:30 +0000

    Added license and changed the comment about where to find a list of contributors.

--- tools/flexitools/flexitools.js
+++ tools/flexitools/flexitools.js
@@ -10,7 +10,7 @@
  *
  * This software is distributed under the terms of the Kupu
  * License. See LICENSE.txt for license text. For a list of Kupu
- * Contributors see CREDITS.txt.
+ * Contributors see CREDITS.txt in the Kupu package.
  *
  *****************************************************************************/
 
