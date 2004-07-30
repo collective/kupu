31f56d27ce35d9e0d0e61ddc650979a4e4c414f0
guido <guido>
2004-07-30 15:14:03 +0000

    Fixed bug in IE, because of the way event handlers were registered to the document returning false from the check method didn't work, now 'event.returnValue' is set to false instead (which seems to work just fine).

--- tools/flexitools/flexitools.js
+++ tools/flexitools/flexitools.js
@@ -16,39 +16,33 @@
 
 
 function fxWritable() {
-    """Tool to make only certain parts of the iframe editable"""
     
     this.fxWritable_status = false;
-    this.fxWritable_MOZ_WARN = false;    
     
     this.initialize = function(editor) {
         /* initialize the tool */
         var doc = editor.getInnerDocument();
-     
+        this.editor = editor; 
         /* set event onKeyPress, onKeyDown */
         addEventHandler(doc, 'keydown', this.checkWrite, this);
+
         if (editor.getBrowserName() == 'Mozilla') {
             addEventHandler(doc, 'keypress', this.checkWrite, this);
         };
     };
    
     this.checkWrite = function(ev) {
-        if (!this.fxWritable_MOZ_WARN) {
-            // commented this out because event.preventDefault() should make
-            // that the user can't even edit anymore
-            // alert('Content outside the areas can not be modified.');
-            this.fxWritable_MOZ_WARN = true;
-        };
         if (this.fxWritable_status) {
             return true;
         } else {
             if (ev.preventDefault) {
                 ev.preventDefault();
+            }else{
+                ev.returnValue = false;
             };
-            return false;
         };
     };
-    
+       
     this.updateState = function(selNode, event) {
         /* evaluate if we are in editable area */
         this.fxWritable_status = false;
