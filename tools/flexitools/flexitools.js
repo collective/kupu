5d7bb7668383504e69f55ba818b3dff21eae9fea
guido <guido>
2004-07-30 13:38:00 +0000

    Fixed indentation, did some reformatting, used kupuhelpers.js where possible and (hopefully) fixed a bug that made typing still possible in Mozilla. Not tested!!

--- tools/flexitools/flexitools.js
+++ tools/flexitools/flexitools.js
@@ -1,6 +1,6 @@
 /*****************************************************************************
  *
- * Copyright (c) 2004 Tomas Hnilica,  tomas.hnilica@webstep.net. 
+ * Copyright (c) 2004 Tomas Hnilica, tomas.hnilica@webstep.net. 
  * All rights reserved.
  *
  * Tool for KUPU, allows edit only parts of document, which are in the
@@ -8,48 +8,61 @@
  * 
  * init: iframe is the edited iframe element
  *
- * The tool works fine in IE5.5+, cannot protect typing in mozilla based 
- * browsers (there is a bug on keydown event). 
+ * This software is distributed under the terms of the Kupu
+ * License. See LICENSE.txt for license text. For a list of Kupu
+ * Contributors see CREDITS.txt.
  *
  *****************************************************************************/
 
 
 function fxWritable(iframe) {
-	
+    
     this.editable = false;
-    var fxWritable_status = false;
-    var fxWritable_MOZ_WARN = false;    
+    this.fxWritable_status = false;
+    this.fxWritable_MOZ_WARN = false;    
     
-     this.initialize = function(editor) {
+    this.initialize = function(editor) {
         /* initialize the tool */
-      var doc = editor.getInnerDocument();
-      this.editable = false;
+        var doc = editor.getInnerDocument();
+        this.editable = false;
      
-     /* set event onKeyPress, onKeyDown */
-     if (_SARISSA_IS_IE) doc.onkeydown = this.checkWrite;
-     if (_SARISSA_IS_MOZ) {
-     		doc.addEventListener('keydown',this.checkWrite, true);
-     		doc.captureEvents(Event.KEYPRESS);
-     		doc.onkeypress = this.checkWrite;
-	}
-   };
-   
-   this.checkWrite = function(ev) {
-   	if (!fxWritable_MOZ_WARN) {alert('Content outside the areas can not be modified.');fxWritable_MOZ_WARN = true;}
-   	if (fxWritable_status) return true;
-	else return false;
-   };
-    
-   this.updateState = function(selNode, event) {
-            /* evaluate if we are in editable area */
-           fxWritable_status = false;
-            var currnode = selNode;
-            while (currnode.nodeName != '#document' && fxWritable_status == false) {
-            	if (currnode.nodeName.toLowerCase()=="div" && currnode.getAttribute("editable")=="yes") fxWritable_status = true;
-                currnode = currnode.parentNode;
-            };
+        /* set event onKeyPress, onKeyDown */
+        addEventHandler(doc, 'keydown', this.checkWrite, this);
+        if (editor.getBrowserName() == 'Mozilla') {
+            addEventHandler(doc, 'keypress', this.checkWrite, this);
         };
-
+    };
+   
+    this.checkWrite = function(ev) {
+        if (!this.fxWritable_MOZ_WARN) {
+            // commented this out because event.preventDefault() should make
+            // that the user can't even edit anymore
+            // alert('Content outside the areas can not be modified.');
+            this.fxWritable_MOZ_WARN = true;
+        };
+        if (this.fxWritable_status) {
+            return true;
+        } else {
+            if (ev.preventDefault) {
+                ev.preventDefault();
+            };
+            return false;
+        };
+    };
+    
+    this.updateState = function(selNode, event) {
+        /* evaluate if we are in editable area */
+        this.fxWritable_status = false;
+        var currnode = selNode;
+        while (currnode.nodeName != '#document' && 
+                this.fxWritable_status == false) {
+            if (currnode.nodeName.toLowerCase()=="div" && 
+                    currnode.getAttribute("editable")=="yes") {
+                this.fxWritable_status = true;
+            };
+            currnode = currnode.parentNode;
+        };
+    };
 };
 
 fxWritable.prototype = new KupuTool;
