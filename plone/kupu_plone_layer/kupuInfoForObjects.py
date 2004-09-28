a1f3e825ee5faf81c7c0225a1d4ca9cb84269a61
duncan <kupuguy@gmail.com>
2004-09-28 11:12:40 +0000

    Added proxy manager role to kupuInforForObjects then put an explicit check for View access before returning each object. Without this you can't browse a folder at all if you don't have access to all objects.

--- plone/kupu_plone_layer/kupuInfoForObjects.py
+++ plone/kupu_plone_layer/kupuInfoForObjects.py
@@ -1,12 +1,16 @@
 ## Script (Python) "kupuInfoForObjects"
-##title=Provide dictionaries with information about a list of objects
 ##bind container=container
 ##bind context=context
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
 ##parameters=values
+##title=Provide dictionaries with information about a list of objects
+##
 from Products.CMFCore.utils import getToolByName
+import AccessControl
+from AccessControl import Unauthorized
+
 kupu_tool = getToolByName(context, 'kupu_library_tool')
 coll_types = kupu_tool.queryPortalTypesForResourceType('collection', ())
 linkbyuid = kupu_tool.getLinkbyuid()
@@ -18,34 +22,50 @@ preview_action = 'kupupreview'
 # should strip the url right back down to resolveuid/whatever.
 base = context.absolute_url()
 
+security = AccessControl.getSecurityManager()
+
 def info(obj):
-    id = obj.getId()
-    portal_type = obj.portal_type
-    collection = portal_type in coll_types
+    if not security.checkPermission('View', obj):
+        return None
 
-    if linkbyuid and not collection and hasattr(obj, 'UID'):
-        url = base+'/resolveuid/%s' % obj.UID()
-    else:
-        url = obj.absolute_url()
+    try:
+        id = obj.getId()
+        portal_type = obj.portal_type
+        collection = portal_type in coll_types
 
-    icon = "%s/%s" % (context.portal_url(), obj.getIcon())
-    width = height = size = None
-    preview = obj.getTypeInfo().getActionById(preview_action, None)
+        if linkbyuid and not collection and hasattr(obj, 'UID'):
+            url = base+'/resolveuid/%s' % obj.UID()
+        else:
+            url = obj.absolute_url()
 
-    if hasattr(obj, 'get_size'):
-        size = context.getObjSize(obj)
-    width = getattr(obj, 'width', None)
-    height = getattr(obj, 'height', None)
-    if callable(width): width = width()
-    if callable(height): height = height()
-        
-    title = obj.Title() or obj.getId()
-    description = obj.Description()
+        icon = "%s/%s" % (context.portal_url(), obj.getIcon())
+        width = height = size = None
+        preview = obj.getTypeInfo().getActionById(preview_action, None)
 
-    return {'id': id, 'url': url, 'portal_type': portal_type,
-          'collection':  collection, 'icon': icon, 'size': size,
-          'width': width, 'height': height,
-          'preview': preview, 'title': title, 'description': description,
-          }
+        try:
+                size = context.getObjSize(obj)
+        except:
+            size = None
 
-return [info(obj) for obj in values]
+        width = getattr(obj, 'width', None)
+        height = getattr(obj, 'height', None)
+        if callable(width): width = width()
+        if callable(height): height = height()
+
+        title = obj.Title() or obj.getId()
+        description = obj.Description()
+
+        return {'id': id, 'url': url, 'portal_type': portal_type,
+              'collection':  collection, 'icon': icon, 'size': size,
+              'width': width, 'height': height,
+              'preview': preview, 'title': title, 'description': description,
+              }
+    except Unauthorized:
+        return None
+
+res = []
+for obj in values:
+    data = info(obj)
+    if data:
+        res.append(data)
+return res
