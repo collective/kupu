2109ba86a5dace45ef536cad6cf7924395c19f9d
guido <guido>
2005-07-13 09:14:38 +0000

    Added toString() call before a .split() on exception, fix for problem with Opera.

--- ecmaunit/ecmaunit.js
+++ ecmaunit/ecmaunit.js
@@ -366,7 +366,7 @@ function HTMLReporter(outputelement, verbose) {
                 var exception = exceptions[i][2].toString();
                 var raw = exceptions[i][3];
                 var div = this.document.createElement('div');
-                var lines = exception.split('\n');
+                var lines = exception.toString().split('\n');
                 var text = this.document.createTextNode(
                     testcase + '.' + attr + ', exception ');
                 div.appendChild(text);
