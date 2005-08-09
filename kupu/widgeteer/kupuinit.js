8e68f097d0c8f0defc7df5ecfe4adb0ff1383224
guido <guido>
2005-08-09 22:56:43 +0000

    Added a distribution (.dist file and a directory) for Widgeteer (http://debris.demon.nl/javascripts/widgeteer), a framework for easily using complex HTML widgets. This should allow integrators to add Kupu to a page by adding a plain textarea (which will also be used transparently as a fallback) to a page and calling a method on some service, which will (if the client supports everything required) replace the textarea with an iframe containing a full Kupu instance.

---
+++ kupu/widgeteer/kupuinit.js
@@ -0,0 +1,225 @@
+/*****************************************************************************
+ *
+ * Copyright (c) 2003-2005 Kupu Contributors. All rights reserved.
+ *
+ * This software is distributed under the terms of the Kupu
+ * License. See LICENSE.txt for license text. For a list of Kupu
+ * Contributors see CREDITS.txt.
+ *
+ *****************************************************************************/
+
+// $Id: kupusilvainit.js 12187 2005-05-11 14:29:58Z guido $
+
+// XXX Port this to the default dist?
+KupuEditor.prototype.afterInit = function() {
+    // select the line after the first heading, if the document is correctly
+    // formatted
+    this.getDocument().getWindow().focus();
+    var doc = this.getInnerDocument();
+    var body = doc.getElementsByTagName('body')[0];
+    var h = null;
+    var iterator = new NodeIterator(body);
+    while (h = iterator.next()) {
+        if (h.nodeType == 1 && h.nodeName.toLowerCase() == 'h2') {
+            var selection = this.getSelection();
+            // okay, the first element node is a h2, select
+            // next node, if it doesn't exist create and select
+            var next = h.nextSibling;
+            if (!next) {
+                next = doc.createElement('p');
+                next.appendChild(doc.createTextNode('\xa0'));
+                body.appendChild(next);
+            } else {
+                var nodeName = next.nodeName.toLowerCase();
+                if (nodeName == 'table') {
+                    next = next.getElementsByTagName('td')[0];
+                } else if (nodeName == 'ul' || nodeName == 'ol') {
+                    next = next.getElementsByTagName('li')[0];
+                };
+            };
+            selection.selectNodeContents(next);
+            selection.collapse();
+            break;
+        } else if (h.nodeType == 1) {
+            break;
+        };
+    };
+    // if we don't first focus the outer window, Mozilla won't show a cursor
+    window.focus();
+    this.getDocument().getWindow().focus();
+};
+
+function initKupu(iframe) {
+    // first we create a logger
+    var l = new DummyLogger();
+
+    // now some config values
+    var conf = loadDictFromXML(document, 'kupuconfig');
+    
+    // the we create the document, hand it over the id of the iframe
+    var doc = new KupuDocument(iframe);
+
+    // now we can create the controller
+    var kupu = new KupuEditor(doc, conf, l);
+    
+    kupu.registerContentChanger(
+        document.getElementById('kupu-editor-textarea'));
+
+    if (kupu.getBrowserName() == 'IE') {
+        // IE supports onbeforeunload, so let's use that
+        addEventHandler(window, 'beforeunload', saveOnPart);
+    } else {
+        // some versions of Mozilla support onbeforeunload (starting with 1.7)
+        // so let's try to register and if it fails fall back on onunload
+        var re = /rv:([0-9\.]+)/
+        var match = re.exec(navigator.userAgent)
+        if (match[1] && parseFloat(match[1]) > 1.6) {
+            addEventHandler(window, 'beforeunload', saveOnPart);
+        } else {
+            addEventHandler(window, 'unload', saveOnPart);
+        };
+    };
+
+    var cm = new ContextMenu();
+    kupu.setContextMenu(cm);
+
+    // now we can create a UI object which we can use from the UI
+    var ui = new KupuUI('kupu-tb-styles');
+    kupu.registerTool('ui', ui);
+
+    var savebuttonfunc = function(button, editor) {editor.saveDocument()};
+    var savebutton = new KupuButton('kupu-save-button', savebuttonfunc);
+    kupu.registerTool('savebutton', savebutton);
+
+    // function that returns a function to execute a button command
+    var execCommand = function(cmd) {
+        return function(button, editor) {
+            editor.execCommand(cmd);
+        };
+    };
+
+    var boldchecker = ParentWithStyleChecker(new Array('b', 'strong'),
+                                             'font-weight', 'bold');
+    var boldbutton = new KupuStateButton('kupu-bold-button', 
+                                         execCommand('bold'),
+                                         boldchecker,
+                                         'kupu-bold',
+                                         'kupu-bold-pressed');
+    kupu.registerTool('boldbutton', boldbutton);
+
+    var italicschecker = ParentWithStyleChecker(new Array('i', 'em'),
+                                                'font-style', 'italic');
+    var italicsbutton = new KupuStateButton('kupu-italic-button', 
+                                           execCommand('italic'),
+                                           italicschecker, 
+                                           'kupu-italic', 
+                                           'kupu-italic-pressed');
+    kupu.registerTool('italicsbutton', italicsbutton);
+
+    var underlinechecker = ParentWithStyleChecker(new Array('u'));
+    var underlinebutton = new KupuStateButton('kupu-underline-button', 
+                                              execCommand('underline'),
+                                              underlinechecker,
+                                              'kupu-underline', 
+                                              'kupu-underline-pressed');
+    kupu.registerTool('underlinebutton', underlinebutton);
+
+    var subscriptchecker = ParentWithStyleChecker(new Array('sub'));
+    var subscriptbutton = new KupuStateButton('kupu-subscript-button',
+                                              execCommand('subscript'),
+                                              subscriptchecker,
+                                              'kupu-subscript',
+                                              'kupu-subscript-pressed');
+    kupu.registerTool('subscriptbutton', subscriptbutton);
+
+    var superscriptchecker = ParentWithStyleChecker(new Array('super', 'sup'));
+    var superscriptbutton = new KupuStateButton('kupu-superscript-button', 
+                                                execCommand('superscript'),
+                                                superscriptchecker,
+                                                'kupu-superscript', 
+                                                'kupu-superscript-pressed');
+    kupu.registerTool('superscriptbutton', superscriptbutton);
+
+    var undobutton = new KupuButton('kupu-undo-button', execCommand('undo'))
+    kupu.registerTool('undobutton', undobutton);
+
+    var redobutton = new KupuButton('kupu-redo-button', execCommand('redo'))
+    kupu.registerTool('redobutton', redobutton);
+
+    var listtool = new ListTool('kupu-list-ul-addbutton', 'kupu-list-ol-addbutton',
+                                'kupu-ulstyles', 'kupu-olstyles');
+    kupu.registerTool('listtool', listtool);
+
+    var dltool = new DefinitionListTool('kupu-list-dl-addbutton');
+    kupu.registerTool('dltool', dltool);
+
+    var imagetool = new ImageTool();
+    kupu.registerTool('imagetool', imagetool);
+
+    var linktool = new LinkTool();
+    kupu.registerTool('linktool', linktool);
+
+    var tabletool = new TableTool(); 
+    kupu.registerTool('tabletool', tabletool);
+
+    var showpathtool = new ShowPathTool();
+    kupu.registerTool('showpathtool', showpathtool);
+
+    var sourceedittool = new SourceEditTool('kupu-source-button',
+                                            'kupu-editor-textarea');
+    kupu.registerTool('sourceedittool', sourceedittool);
+
+    var spellchecker = new KupuSpellChecker('kupu-spellchecker-button',
+                                            'kupu_spellcheck');
+    kupu.registerTool('spellchecker', spellchecker);
+
+    var viewsourcetool = new ViewSourceTool();
+    kupu.registerTool('viewsourcetool', viewsourcetool);
+    
+    // Function that returns function to open a drawer
+    var opendrawer = function(drawerid) {
+        return function(button, editor) {
+            drawertool.openDrawer(drawerid);
+        };
+    };
+
+    var imagelibdrawerbutton = new KupuButton('kupu-imagelibdrawer-button',
+                                              opendrawer('imagelibdrawer'));
+    kupu.registerTool('imagelibdrawerbutton', imagelibdrawerbutton);
+
+    var linklibdrawerbutton = new KupuButton('kupu-linklibdrawer-button',
+                                             opendrawer('linklibdrawer'));
+    kupu.registerTool('linklibdrawerbutton', linklibdrawerbutton);
+
+    // create some drawers, drawers are some sort of popups that appear when a 
+    // toolbar button is clicked
+    var drawertool = new DrawerTool();
+    kupu.registerTool('drawertool', drawertool);
+
+    var linklibdrawer = new LinkLibraryDrawer(linktool, conf['link_xsl_uri'],
+                                              conf['link_libraries_uri'],
+                                              conf['link_images_uri']);
+    drawertool.registerDrawer('linklibdrawer', linklibdrawer);
+
+    var imagelibdrawer = new ImageLibraryDrawer(imagetool, conf['image_xsl_uri'],
+                                                conf['image_libraries_uri'],
+                                                conf['search_images_uri']);
+    drawertool.registerDrawer('imagelibdrawer', imagelibdrawer);
+    
+//    var nonxhtmltagfilter = new NonXHTMLTagFilter();
+//    kupu.registerFilter(nonxhtmltagfilter);
+
+    kupu.xhtmlvalid.setAttrFilter(['is_toc', 'toc_depth', 'is_citation', 
+                                    'source', 'author', 'source_id', 
+                                    'silva_type', 'alignment', 
+                                    'link_to_hires', 'link']);
+    // allow all attributes on div, since ExternalSources require that
+    kupu.xhtmlvalid.includeTagAttributes(['div'], ['*']);
+    kupu.xhtmlvalid.includeTagAttributes(['p'], ['silva_type']);
+    kupu.xhtmlvalid.includeTagAttributes(['h6'], ['silva_type']);
+    kupu.xhtmlvalid.includeTagAttributes(['img'], ['alignment', 
+                                            'link_to_hires', 
+                                            'target', 'link']);
+
+    return kupu;
+};
