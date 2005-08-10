72513da74771e591c20d6eeaa3d0fc9da540182c
guido <guido>
2005-08-10 22:06:29 +0000

    Added some code that makes drawers appear in the parent window rather than in the window containing the editor. This works for 'sync' drawers, but will need some more attention for the library ones.

--- widgeteer/kupustart.js
+++ widgeteer/kupustart.js
@@ -33,28 +33,6 @@ function startKupu() {
     var frame = getFromSelector('kupu-editor'); 
     var kupu = initKupu(frame);
     
-    // this makes the editor's content_changed attribute set according to changes
-    // in a textarea or input (registering onchange, see saveOnPart() for more
-    // details)
-    kupu.registerContentChanger(getFromSelector('kupu-editor-textarea'));
-
-    // let's register saveOnPart(), to ask the user if he wants to save when 
-    // leaving after editing
-    if (kupu.getBrowserName() == 'IE') {
-        // IE supports onbeforeunload, so let's use that
-        addEventHandler(window, 'beforeunload', saveOnPart);
-    } else {
-        // some versions of Mozilla support onbeforeunload (starting with 1.7)
-        // so let's try to register and if it fails fall back on onunload
-        var re = /rv:([0-9\.]+)/
-        var match = re.exec(navigator.userAgent)
-        if (match[1] && parseFloat(match[1]) > 1.6) {
-            addEventHandler(window, 'beforeunload', saveOnPart);
-        } else {
-            addEventHandler(window, 'unload', saveOnPart);
-        };
-    };
-
     // and now we can initialize...
     kupu.initialize();
 
