1a7162d59a19e53eb268880da764e0534377c2e2
duncan <kupuguy@gmail.com>
2005-11-04 09:09:41 +0000

    'u' tag should not be filtered by default. See http://members.plone.org/collector/4873

--- plone/plonelibrarytool.py
+++ plone/plonelibrarytool.py
@@ -61,7 +61,7 @@ _default_resource_types = {
 
 # Tidy up html by exlcluding lots of things.
 _excluded_html = [
-  (('center', 'span', 'tt', 'big', 'small', 'u', 's', 'strike', 'basefont', 'font'), ()),
+  (('center', 'span', 'tt', 'big', 'small', 's', 'strike', 'basefont', 'font'), ()),
   ((), ('dir','lang','valign','halign','border','frame','rules','cellspacing','cellpadding','bgcolor')),
   (('table','th','td'),('width','height')),
 ]
