5d6a07f5a2cde7607dda65eab9d84cef6f9d4aee
fschulze <fschulze>
2008-07-23 10:50:40 +0000

    Finished the egg.

---
+++ Products.kupu/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join("Products", "kupu", "version.txt")).read()
+version = version.replace('kupu', '').strip()
+
+setup(name='Products.kupu',
+      version=version,
+      description="",
+      long_description=open(os.path.join("Products", "kupu", "doc", "README.txt")).read() + "\n" +
+                       open(os.path.join("Products", "kupu", "doc", "CHANGES.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Kupu Team',
+      author_email='kupu-dev@codespeak.net',
+      url='http://kupu.oscom.org/',
+      license='Kupu License',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
