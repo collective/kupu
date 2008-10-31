45ff6e09fe279444a2fb68079d852628f169a47d
Michiel Meeuwissen <git@meeuw.org>
2008-10-31 13:42:42 +0000

    uploading of attachments was a bit broken

--- mmbase/mmbasedrawers.js
+++ mmbase/mmbasedrawers.js
@@ -1,7 +1,7 @@
 /**
  * Specialized drawer for mmbase-resources.
  * Like LinkLibraryDrawer, but with upload-functionality.
- * $Id $
+ * $Id: $
  */
 
 function ResourceLibraryDrawer(tool, xsluri, libsuri, searchuri, baseelement) {
@@ -21,12 +21,15 @@ function ResourceLibraryDrawer(tool, xsluri, libsuri, searchuri, baseelement) {
         var selxpath = '//resource[@selected]';
         var selnode = this.xmldata.selectSingleNode(selxpath);
         if (!selnode) {
-            var uploadbutton = this.xmldata.selectSingleNode("/libraries/*[@selected]/uploadbutton");
+            var uploadbutton = this.xmldata.selectSingleNode("/libraries/*[@selected]//uploadbutton");
             if (uploadbutton) {
                 this.uploadResource();
             }
             return;
+
         }
+
+        //Not sure if the following few lines of code make any sense.
 
         var uri = selnode.selectSingleNode('uri/text()').nodeValue;
         uri = uri.strip();  // needs kupuhelpers.js
@@ -34,8 +37,6 @@ function ResourceLibraryDrawer(tool, xsluri, libsuri, searchuri, baseelement) {
         title = selnode.selectSingleNode('title/text()').nodeValue;
         title = title.strip();
 
-        // XXX requiring the user to know what link type to enter is a
-        // little too much I think. (philiKON)
         var type = null;
         var elm = getFromSelector('link_name');
         var name = elm ? elm.value : '';
@@ -49,16 +50,6 @@ function ResourceLibraryDrawer(tool, xsluri, libsuri, searchuri, baseelement) {
     // upload, on submit/insert press
     this.uploadResource = function() {
         var form = document.getElementById('kupu_upload_form');
-        if (!form || form.node_prop_image.value == '') {
-            alert("" + form + " " + form.node_prop_image.value);
-            return;
-        }
-
-        if (form.node_prop_title.value == "") {
-            alert("Please enter a title for the image you are uploading");
-            return;
-        };
-
         form.submit();
     };
 
@@ -73,11 +64,12 @@ function ResourceLibraryDrawer(tool, xsluri, libsuri, searchuri, baseelement) {
     };
 
     // called by onLoad within document sent by server
-    this.finishUpload = function(url, mimetype) {
+    this.finishUpload = function(url, mimetype, title) {
         this.editor.resumeEditing();
+        kupu.content_changed = true;
         var form = document.getElementById('kupu_upload_form');
-        var title = "[" + mimetype + ":" + form.node_prop_title.value + "]";
-        this.tool.createLink(url, null, null, null, title);
+        var t = "[" + mimetype + ":" + title+ "]";
+        this.tool.createLink(url, null, null, null, t);
         this.shared.newimages = 1;
         this.drawertool.closeDrawer();
     };
