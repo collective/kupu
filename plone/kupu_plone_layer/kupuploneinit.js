93dc1f267ee225dadc519d2a511bb7dd17544b42
dannyb <danny.bloemendaal@informaat.nl>
2009-07-23 14:37:24 +0000

    Don't assume there is only one kupu widget on the page. Made each config node unique.

--- plone/kupu_plone_layer/kupuploneinit.js
+++ plone/kupu_plone_layer/kupuploneinit.js
@@ -26,7 +26,7 @@ function initPloneKupu(editorId) {
     var l = new DummyLogger();
 
     // now some config values
-    document.getElementById('kupu-config').innerHTML = unescape(document.getElementById('kupu-config-escaped').innerHTML);
+    document.getElementById(editorId + '-config').innerHTML = unescape(document.getElementById(editorId + '-config-escaped').innerHTML);
     var conf = loadDictFromXML(document, prefix+'xml.kupuconfig');
 
     // the we create the document, hand it over the id of the iframe
